4d883a21b2402ca1c34b68cb7ca1c98b6ac6b207
madewole1 <81781629+madewole1@users.noreply.github.com>
2021-04-02 00:32:04 -0400

    Update umd_student_platform.py
    added a class and init function to umd_student_platform.py

--- umd_student_platform.py
+++ umd_student_platform.py
@@ -1,8 +1,15 @@
 """A platform for UMD students to look up prerequistes for classes
 """
 
-class student():
-  """ Whichever student submits information
-  
-  Attributes:
+class Student:
+    """This class will take the student't name, as well as their prerequisites.
+    
+    Attributes:
+        name(str): name of student 
+        prerequisites(set): prerequisites the student has 
+    """
+    def __init__(self, name, prerequistes):
+       self.name = name
+       self.prerequistes = set()
+
       
